e5d458e23227ddbaa9605117d49f5da9de09a1ba
LIMGEES <dlawlghks04@nate.com>
2024-01-16 20:48:45 +0900

    Changes 1.4

--- src/main/java/com/example/firstproject/controller/ArticleController.java
+++ src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -6,8 +6,14 @@ import com.example.firstproject.repository.ArticleRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 @Slf4j
@@ -31,5 +37,25 @@ public class ArticleController {
         log.info(saved.toString());
         //System.out.println(saved.toString());
         return "";
+    }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id="+id);
+        //1.id를 조회해 데이터 가져오기
+        Article articleEntity =aricleRepository.findById(id).orElse(null);
+        Optional<Article> article = aricleRepository.findById(id);
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("aricle",articleEntity);
+        //3. 뷰 페이지 반환하기
+        return "articles/show";
+    }
+    @GetMapping("/articles")
+    public String index(Model model){
+        //1. 모든 데이터 가져오기
+        ArrayList<Article> articleEntityList = aricleRepository.findAll();
+        //2. 모델에 데이터 등록하기
+        model.addAttribute("articleList",articleEntityList);
+        //3. 뷰 페이지 설정하기
+        return "articles/index";
     }
 }
--- src/main/java/com/example/firstproject/repository/ArticleRepository.java
+++ src/main/java/com/example/firstproject/repository/ArticleRepository.java
@@ -3,7 +3,10 @@ package com.example.firstproject.repository;
 
 import org.springframework.data.repository.CrudRepository;
 import com.example.firstproject.entity.Article;
+
+import java.util.ArrayList;
+
 public interface ArticleRepository extends CrudRepository<Article , Long> {
-
-
+    @Override
+    ArrayList<Article> findAll();
 }
